9a9d588d07f4d208ab7c1af9de0867aaf95ca574
prabhulesllin <prabhuleslin@gmail.com>
2017-01-26 12:28:45 +0530

    Project created

---
+++ VersionControlTest/VersionControlTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VersionControlTest
+//
+//  Created by Prabhu on 1/26/17.
+//  Copyright © 2017 Tringapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
